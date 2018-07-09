15ba9b2e21ce6955f5c586802a01d99cba4e3cc6
KennyVillanueva <kennyvillanueva15@gmail.com>
2018-07-08 22:21:46 -0500

    FINAL PROJECT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  tempindexvalue(arg0: any, arg1: any): any {
+    throw new Error("Method not implemented.");
+  }
+  name = 'kenny';
+  todoInput:' ';
+  Todos= [ ];
+  deleteSelect= '';
+createTodo() {
+  this.Todos.push(this.todoInput);
+  this.todoInput = " ";
+
+}
+editTodo(chore) {
+  console.log('chore was click:' + chore);
+  let index = this.Todos.indexOf(chore);
+  console.log('index of that chore:' 
++
+index);
+this.Todos[index] = prompt('please write a new todo')
+}
+deleteTodo(chore) {
+  let index = this.Todos.indexOf(chore);
+  this.Todos.splice(index, 1);
+}
+
 }
